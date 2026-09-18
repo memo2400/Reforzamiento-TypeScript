ea0585e4bd889fb73ff53e2f58108c3a1941e212
Guillermo Rosas <memorosas.00@gmail.com>
2026-09-18 15:41:07 -0600

    feat: fin leccion 13 tipo de datos
    Lecc 13

--- 01-reforzamiento/src/bases/01-const-let.ts
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -1,10 +1,16 @@
 
 // var no usar esto nunca
 
-const firstName = 'Jens';
+const firstName : string = 'Jens';
 const lastName = "Johannson";
+
+// aqui si infiere que es texto real a secas
+let variableStringReal = "texto";
+
+let diceNumber = 5;
+//diceNumber = '3';   // esto es un error pero typescrip si lo ejecuta
 
 const containsLetterH = firstName.includes('h');
 
-console.log({containsLetterH}); // this kind of impresion show me the anme an d value in the console.
+console.log({ containsLetterH, diceNumber }); // this kind of impresion show me the anme an d value in the console.
 
