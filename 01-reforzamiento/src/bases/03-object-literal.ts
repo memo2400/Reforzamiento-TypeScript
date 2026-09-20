acd90d3317027330ad454de93e1c58cf739df0c9
guillermo rosas <memorosas.00@gmail.com>
2026-09-19 20:46:18 -0600

    feat: fin leccion 15 objetos

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -4,14 +4,28 @@
 const person = {
     firstName: "Jens",
     lastName: "Johannson",
-    age: 45
+    age: 45,
+    address: {
+        zipcode: 28080,
+        city: "Helsinki"
+    }
 };
 
 //person = "nueva cosa"; // esto esta mal
 
+// esto error ambos sonigualitos, si cambio uno se cambia el otro
+// const spiderman = person;
+
+// aqui si que copiamos, pero solo las cabeceras, pero del primer nivel
+// const spiderman = {...person};
+
+const spiderman = structuredClone(person);
+
 // si que puedo moficicar las properties de este objeto, pero no puedo reasignar el objeto completo
 
-person.firstName = "Kai";
-person.lastName = "Hansen"; 
+spiderman.firstName = "Kai";
+spiderman.lastName = "Hansen";
+// usando spred sencillo, este no funciona como queremos.
+spiderman.address.city = "Hamburgo"; 
 
-console.log(person);
+console.log(person, spiderman);
