c17b11a1318ae0ef835352b287e98b7802ec9cc6
Guillermo Rosas <memorosas.00@gmail.com>
2026-09-25 15:44:39 -0600

    feat: continuamos con desestructuracion
    Lecc 20

--- 01-reforzamiento/src/bases/06-obj-destructuring.ts
+++ 01-reforzamiento/src/bases/06-obj-destructuring.ts
@@ -10,7 +10,39 @@ const person = {
 // esto es ineficiente
 const name1 = person.name;
 
-// Esto es pro
+// Esto es pro, podemos poner en desorden
 const { name, age } = person;
 
 console.log(name, age);
+
+interface Hero {
+    name: string;
+    age: number;
+    key: string;
+    rank?: string;  //asi existe o es indefinido
+}
+
+const useContex = ( {name, age, key, rank = "sin rango"} :Hero) => {
+
+    return {
+      keyName: key,
+      user: {
+        name: name,
+        // esto es mas rapido en JS y TS, para no repetir
+        age,
+      },
+      rank: rank,
+    };
+
+};
+
+const context = useContex(person);
+
+console.log(context);
+
+// podemos acceder de esta forma al retorno que contex
+context.user.age;
+
+const { keyName, rank, user } = useContex(person);
+console.log("/ 3 /");
+console.log(keyName, rank, user);
