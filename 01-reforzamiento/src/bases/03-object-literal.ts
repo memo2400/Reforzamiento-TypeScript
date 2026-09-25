b7b2926a81d35afb3e9984318ec352ebf39931e9
guillermo rosas <memorosas.00@gmail.com>
2026-09-24 21:46:05 -0600

    feat: Objetos clonados fin lecc 15

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,15 +1,28 @@
 
+// con esta interface moldeamos el objeto que queremos,
+// la interface solo existe en TS, al convertirse en JS no existe.
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
 
+    address?: { //con el ? podemos hacer esta parte opcional
+        zipcode: string;
+        city: string;
+    }
+}
 
-const person = {
+//heredamos la interface    
+const person: Person = {
     firstName: "Jens",
     lastName: "Johannson",
-    age: 45,
-    address: {
-        zipcode: 28080,
+    age: 45,      // poner edad en string puede ser un error de dedo
+    address: {      // si adress no se declara en la interface se marca error
+        zipcode: '28080',
         city: "Helsinki"
     }
 };
+
 
 //person = "nueva cosa"; // esto esta mal
 
@@ -26,6 +39,6 @@ const spiderman = structuredClone(person);
 spiderman.firstName = "Kai";
 spiderman.lastName = "Hansen";
 // usando spred sencillo, este no funciona como queremos.
-spiderman.address.city = "Hamburgo"; 
+// spiderman.address?.city = "Hamburgo"; 
 
 console.log(person, spiderman);
